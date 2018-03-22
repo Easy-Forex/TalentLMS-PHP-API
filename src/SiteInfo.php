27b6af6950d00ee7c27336ded424c2f1263b156b
dtoskas <37575203+dtoskas@users.noreply.github.com>
2018-03-22 15:24:18 +0200

    Us 11084 create talent lms lib (#1)
    * Install TalentLMS through composer and autoload (US #11084)
    
    * Install TalentLMS through composer and autoload (US #11084)
    
    * Remove hardcoded credentials from test case (US #11084)

---
+++ src/SiteInfo.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace TalentLMS;
+
+class Siteinfo extends ApiResource
+{
+
+    public static function get()
+    {
+        $class = get_class();
+        return self::_scopedAll($class);
+    }
+
+    public static function getRateLimit()
+    {
+        $class = get_class();
+        return self::_scopedGetRateLimit($class);
+    }
+
+    public static function getTimeline($params)
+    {
+        $class = get_class();
+        return self::_scopedGetTimeline($class, $params);
+    }
+}
---
+++ src/TalentLMS.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace TalentLMS;
+
+abstract class TalentLMS
+{
+    public static $apiKey;
+    public static $domain;
+    public static $apiBase;
+    const VERSION = '1.0';
+
+    public static function getApiKey()
+    {
+        return self::$apiKey;
+    }
+
+    public static function setApiKey($apiKey)
+    {
+        self::$apiKey = $apiKey;
+    }
+
+    public static function getDomain()
+    {
+        return self::$domain;
+    }
+
+    public static function setDomain($domain)
+    {
+        $domain = str_replace('http://', '', $domain);
+        $domain = str_replace('https://', '', $domain);
+        $domain = str_replace('/', '', $domain);
+
+        self::$domain = $domain;
+        self::$apiBase = 'http://' . $domain . '/api/v1';
+    }
+
+    public static function getApiBase()
+    {
+        return self::$apiBase;
+    }
+
+    public static function setApiBase($apiBase)
+    {
+        self::$apiBase = $apiBase;
+    }
+}
