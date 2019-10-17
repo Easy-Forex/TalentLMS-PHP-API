4d655af3217da0a55b9ff7ef7196b8cbff729756
dimitris <dimitrios@easymarkets.com>
2019-10-17 16:03:42 +0300

    Adding new object for languages mapping (US #14527)

--- src/User.php
+++ src/User.php
@@ -4,25 +4,6 @@ namespace TalentLMS;
 
 class User extends ApiResource
 {
-	/**
-	 * Mapping method for the language key between easymarkets and talent lms
-	 * Available keys on TalentLMS 
-	 * en,es,de,fr,el,it,ru,pl,hu,da,sv,no,et,lt,sr,lv,cs,sl,tr,pt,zh_cn,zh_tw,nl,ja,hy,hr,bs_ba,he,ar_ae,fa_ir,mn,ko,id,th,pt_br,ro,fi
-	 * @param $easymarketsKey
-	 * @return mixed|null
-	 */
-	public static function getTalentLmsLanguageKey($easymarketsKey)
-	{
-		$languageMapping = array(
-			'en'      => 'en',
-			'zh-hans' => 'zh_cn',
-			'pl'      => 'pl',
-			'ar'      => 'ar_ae',
-			'es'      => 'es',
-			'de'      => 'de'
-		);
-		return isset($languageMapping[$easymarketsKey])?$languageMapping[$easymarketsKey]:null;	
-	}
 
     public static function retrieve($params)
     {
